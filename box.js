89a162cc4b2960f4e0126c2bed208af336f2fb3a
Vivaan14 <vivaanagrawal140208@gmail.com>
2020-12-22 14:10:26 +0530

    29

--- box.js
+++ box.js
@@ -1,20 +1,22 @@
-class block{
+class Block {
     constructor(x, y, width, height) {
         var options = {
           // isStatic:true,
-            restitution :0.1,
-            friction :5,
+            restitution :0.4,
+            friction :0.0
            
            
         }
-        this.visiblity = 225;
-        this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
         this.height = height;
+        this.Visiblity = 225;
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        //this.width = width;
+        //  this.height = height;
         World.add(world, this.body);
       }
       display(){
-        if(this.body.speed < 10){
+        if(this.body.speed < 7){
         
         var angle = this.body.angle;
         var pos= this.body.position;
