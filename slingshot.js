064617609d3c113cd91bfcc346691c889b8f7ec0
Vivaan14 <vivaanagrawal140208@gmail.com>
2020-12-22 14:13:27 +0530

    TS

--- slingshot.js
+++ slingshot.js
@@ -5,13 +5,17 @@ class SlingShot{
             bodyA: bodyA,
             pointB: pointB,
             stiffness: 0.04,
-            length: 10
+            length: 1
         }
         this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
     }
-   
+   attach(body){
+    this.sling.bodyA = body;
+   }
+
+
     fly(){
         this.sling.bodyA = null;
     }
